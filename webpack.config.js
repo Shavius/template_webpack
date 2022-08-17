aff1b8ac6a55193d74c8fab62b115b0f21a05f95
Shava <Shava@ukr.net>
2022-08-17 19:28:37 +0300

    config devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,54 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 let mode = 'development';
+let devtool = 'source-map';
 
 if (process.env.NODE_ENV === 'production') {
-  mode = 'production';
+	mode = 'production';
+	devtool = 'none';
 }
 
 module.exports = {
-    mode: mode,
+	mode: mode,
+	devtool: devtool,
 
-    entry: path.resolve(__dirname, 'src', 'js/script.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        filename: 'js/script.js'
-    },
+	entry: path.resolve(__dirname, 'src', 'js/script.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+		filename: 'js/script.js'
+	},
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-        })
-    ],
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'src', 'index.html'),
+		})
+	],
 
-    module: {
-        rules: [
-          {
-            test: /\.js$/i,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-            },
-          },
-          {
-            test: /\.html$/i,
-            loader: "html-loader",
-          },
-        ],
-      },
+	module: {
+		rules: [
+			{
+				test: /\.js$/i,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+		],
+	},
+
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist'),
+		},
+		client: {
+			overlay: true,
+		},
+		port: 3000,
+		hot: true,
+	}
 }
