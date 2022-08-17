ba48d621b0ad684139f3b35afb10f2e1ff0a427b
Shava <Shava@ukr.net>
2022-08-17 17:42:33 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/js/script.js'),
+    entry: path.resolve(__dirname, 'src', 'js/script.js'),
     output: {
-        path: resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist'),
         clean: true,
-        filename: 'script.js'
+        filename: 'js/script.js'
     }
 }
