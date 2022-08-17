cb7bf5d03be43ffbb2bb4611f6d3b74aa6766a79
Shava <Shava@ukr.net>
2022-08-17 18:45:58 +0300

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,13 @@ module.exports = {
     module: {
         rules: [
           {
+            test: /\.js$/i,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            },
+          },
+          {
             test: /\.html$/i,
             loader: "html-loader",
           },
