199b29dd5607568e4e508c1617344e9323329b84
Shava <Shava@ukr.net>
2022-08-31 19:07:32 +0300

    change prettier and eslint options

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,12 @@
 module.exports = {
-    testEnvironment: "jsdom",
-    collectCoverageFrom: [
-        "src/js/modules/*.js",
-        "!**/node_modules/**",
-    ],
-    coverageThreshold: {
-        global: {
-            branches: 80,
-            functions: 80,
-            lines: 80,
-            statements: 80,
-        },
-    },
+	testEnvironment: "jsdom",
+	collectCoverageFrom: ["src/js/modules/*.js", "!**/node_modules/**"],
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: 80,
+		},
+	},
 };
